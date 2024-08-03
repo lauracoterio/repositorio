dba14d348fbe25de013647fdaeb5e18ec45c3c82
lauracoterio <lauracoterio3009@gmail.com>
2024-08-03 15:37:47 -0500

    dios?

---
+++ repositorio1.php
@@ -0,0 +1,16 @@
+<?php
+$titulo = "HOLA MUNDOOOOOOOOOOOOO ♡ ";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    </head>
+ 
+    <body>
+        <h1><?php echo $titulo ?></h1>
+    </body>
+    </html>
